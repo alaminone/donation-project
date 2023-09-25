2fbb273113bf41dd3cd5f41953eed9a6fd870b6e
Md. Naoshad Hossain <naoshad@Mds-MacBook-Pro.local>
2023-09-25 17:41:30 +0600

    finishing show the donation deteils

--- src/Pagess/showDeteils/ShowDeteils.jsx
+++ src/Pagess/showDeteils/ShowDeteils.jsx
@@ -1,33 +1,46 @@
 import { useLoaderData, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ShowDeteils = () => {
   const alldeteils = useLoaderData();
   const { id } = useParams();
   const intId = parseInt(id);
   const showDonationDeteils = alldeteils.find(donationdetails => donationdetails.id === intId);
-  const { title, img, description, price } = showDonationDeteils;
+  const { title, img, description, price,button_bg,title_text } = showDonationDeteils;
 
-  const imageWrapperStyle = {
-    opacity: 0.7, // Adjust the opacity value as needed
-  };
+ const handelDonationBtn = () =>{
+    toast.error("Thanks for your Donation", {
+        position: "top-center",
+        autoClose: 2000,
+      });
+ }
 
   return (
     <div className='container mx-auto '>
-      <div className="card card-compact  bg-base-100 mx-10 my-10">
-        <div className="hero-overlay bg-opacity-90"></div>
-        <figure>
-          <div style={imageWrapperStyle}>
+      <div className="card card-compact  bg-base-100 mx-2 my-10">
+        
+       
+        <div className='relative'>
+        <div>
             <img className='h-min w-full rounded-lg' src={img} alt="Shoes" />
           </div>
-        </figure>
-        <div className="">
-          <button className="btn btn-primary h-10 w-full">{price}</button>
+       
+        <div style={{ backgroundColor: button_bg }} className="absolute bottom-0 py-7 w-full bg-opacity-10 bg-black  ">
+         
+         <button
+         onClick={handelDonationBtn}
+          style={{ backgroundColor: title_text }} 
+          className="btn btn-outline ml-7 py-4 px-7 text-white">Donate ${price}</button>
+        
+        </div>
         </div>
         <div className="card-body">
           <h2 className="card-title text-5xl font-bold mt-5">{title}</h2>
           <p className="text-sm text-gray-500 mt-5">{description}</p>
         </div>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
